f8ec7cd610cf5b08476915cb2228a45348adf17a
a-z-nath <at.avijit.deb@gmail.com>
2025-09-24 00:35:56 +0600

    [Sever] fix: remove redundant JWT verification from individual quiz routes

--- server/src/routes/topic-route.ts
+++ server/src/routes/topic-route.ts
@@ -5,25 +5,29 @@ import {
   createQuiz,
   generateLearningContent,
   generateQuiz,
+  generateShortQuestions,
   getExternalResources,
   getQuizById,
   getQuizzesByTopic,
-  getStarterFeed
+  getStarterFeed,
 } from '../controllers/topic-controllers';
 import { verifyJWT } from '../middleware/auth-middleware';
 
 const topic_router = Router();
 
+topic_router.use(verifyJWT);
+//below are all protected routes
 topic_router.route('/generate-topic').post(createAllTopics);
 topic_router.route('/generate-content').post(generateLearningContent);
-topic_router.route('/generate-quiz').post(verifyJWT, generateQuiz);
+topic_router.route('/generate-quiz').post(generateQuiz);
 topic_router.route('/external-resources').post(getExternalResources);
 
 topic_router.route('/starter-feed').get(getStarterFeed);
+topic_router.route('/generate-short-questions').post(generateShortQuestions);
 
-topic_router.route('/create-quiz').post(verifyJWT, createQuiz);
-topic_router.route('/submit-quiz').post(verifyJWT, submitQuiz);
-topic_router.route('/quizzes/:topicId').get(verifyJWT, getQuizzesByTopic);
-topic_router.route('/quiz/:quizId').get(verifyJWT, getQuizById);
+topic_router.route('/create-quiz').post(createQuiz);
+topic_router.route('/submit-quiz').post(submitQuiz);
+topic_router.route('/quizzes/:topicId').get(getQuizzesByTopic);
+topic_router.route('/quiz/:quizId').get(getQuizById);
 
 export default topic_router;
